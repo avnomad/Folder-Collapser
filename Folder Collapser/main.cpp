82128e7907bab4df8d8de98a6660c47a7ca2d403
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-27 16:40:35 +0300

    Added copyright notices and license.

--- Folder Collapser/main.cpp
+++ Folder Collapser/main.cpp
@@ -1,9 +1,28 @@
+//	Copyright (C) 2008, 2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+/*
+ *	This file is part of Folder Collapser.
+ *
+ *	Folder Collapser is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Folder Collapser is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Folder Collapser.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::cerr;
 using std::endl;
+
 #include <stdlib.h>
 #include <string.h>
 
